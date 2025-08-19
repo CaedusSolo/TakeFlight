84a9712cfc2f0c92ae70611c60e4ecfa9de4deb3
Ying Tong <yingtong0106@gmail.com>
2025-08-19 13:09:03 +0800

    feat: Added more auth functions to supabase auth templates

--- templates/auth/nextjs-auth/nextauth/nextAuth-config.ts
+++ templates/auth/nextjs-auth/nextauth/nextAuth-config.ts
@@ -1,7 +1,43 @@
-import { NextAuthConfig } from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google"
 
-export default {
-  providers: [GitHub],
-  pages: { signIn: '/auth/login' }
-};
+export const authConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET
+    })
+  ],
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error"
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account?.provider === 'github') {
+        return true
+      }
+      return false
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = 'user';
+      }
+      return token
+    }, 
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string
+      }
+      return session
+    }
+  },
+  session: { strategy: 'jwt'},
+} satisfies NextAuthConfig
